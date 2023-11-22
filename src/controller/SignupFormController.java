72455ccd1f61faf37d9ba50a03546037c9ca5d8f
rusiruchapana <rusiruchapana@gmail.com>
2023-11-22 16:58:13 +0530

    code for  the sign up page.

--- src/controller/SignupFormController.java
+++ src/controller/SignupFormController.java
@@ -1,16 +1,25 @@
 package controller;
 
 import db.DB_Connect;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignupFormController {
     public Button btnRgstr;
@@ -19,9 +28,11 @@ public class SignupFormController {
     public TextField fldEmail;
     public PasswordField fldConfirmPassword;
     public Label lblUsernameError;
-
-
-
+    public Label lblEmailError;
+    public Label lblPasswordError;
+    public Label lblConfirmPasswordError;
+    public Label lblClickLogin;
+    public AnchorPane root;
 
 
     Connection conn=null;
@@ -32,8 +43,13 @@ public class SignupFormController {
     public void initialize() {
         conn= DB_Connect.connect();
     }
-    
-    
+
+
+
+
+
+
+
 
     public void btnRgstrOnMouseClicked(MouseEvent mouseEvent) {
         String username= fldUserName.getText();
@@ -50,18 +66,88 @@ public class SignupFormController {
             }
 
             if(email.isEmpty()){
-                
+                lblEmailError.setText("Email is empty.");
+                errors++;
+            }else{
+                    String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+                    Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+                    Matcher matcher = pattern.matcher(email);
+                    boolean isvalid = matcher.matches();
+
+                    if(!isvalid){
+                        lblEmailError.setText("Email is not a valid Email.");
+                        errors++;
+                    }
+
             }
 
 
 
-            String sql= "insert into operation (username,email,password,confirmPassword) values('"+username+"','"+email+"','"+password+"','"+confirmPassword+"')";
-            pst= conn.prepareStatement(sql);
-            pst.execute();
+            if(password.isEmpty()){
+                    lblPasswordError.setText("Password is Empty.");
+                    errors++;
+            }
+
+
+
+
+            if(confirmPassword.isEmpty()){
+                    lblConfirmPasswordError.setText("Confirm password is Empty.");
+                    errors++;
+            }else{
+                    if(!confirmPassword.equals(password)){
+                          lblConfirmPasswordError.setText("Given 2 passwords are not same.");
+                          errors++;
+                    }
+            }
+
+
+            if(errors==0){
+
+                    String query ="select * from operation where email=?";
+                    pst = conn.prepareStatement(query);
+                    pst.setString(1, fldEmail.getText());
+                    rs= pst.executeQuery();
+
+                    if(rs.next()){
+                            JOptionPane.showMessageDialog(null, "Already exist this user");
+                    }else{
+                            String sql= "insert into operation (username,email,password,confirmPassword) values('"+username+"','"+email+"','"+password+"','"+confirmPassword+"')";
+                            pst= conn.prepareStatement(sql);
+                            pst.execute();
+
+                            lblUsernameError.setText("");
+                            lblEmailError.setText("");
+                            lblPasswordError.setText("");
+                            lblConfirmPasswordError.setText("");
+
+                            JOptionPane.showMessageDialog(null, "Log in Successfully.");
+                    }
+
+            }else {
+                JOptionPane.showMessageDialog(null, "Fields data's are not filled correctly.");
+            }
+
+
 
         } catch (SQLException throwables) {
-            throwables.printStackTrace();
+                throwables.printStackTrace();
         }
+
+
+    }
+
+    public void lblClickLoginOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+                Parent parent = FXMLLoader.load(this.getClass().getResource("../view/LoginForm.fxml"));
+                Scene scene = new Scene(parent);
+
+                Stage stage= (Stage) root.getScene().getWindow();
+                stage.setScene(scene);
+                stage.setTitle("Login Form");
+                stage.centerOnScreen();
+                stage.show();
+
 
 
     }
