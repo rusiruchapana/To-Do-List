df68b31ba524b1a689eac697b1f32d7c1ac9c30e
rusiruchapana <rusiruchapana@gmail.com>
2023-11-28 21:44:44 +0530

    login form to code

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardFormController {
+    public AnchorPane root;
+
+    public void btnLogoutOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        Parent parent = FXMLLoader.load(this.getClass().getResource("../view/LoginForm.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Login form");
+        stage.centerOnScreen();
+        stage.show();
+
+
+
+
+
+
+
+
+    }
 }
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -29,6 +29,13 @@ public class LoginFormController {
     public Label lblUsernameError;
     public Label lblPasswordError;
 
+    Connection conn = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+
+    public void initialize(){
+            conn = DB_Connect.connect();
+    }
 
 
     public void lblRegisterOnMouseClicked(MouseEvent mouseEvent) throws IOException {
@@ -40,14 +47,11 @@ public class LoginFormController {
             stage.setTitle("Register form");
             stage.centerOnScreen();
             stage.show();
-
-
-
-
-
     }
 
-    public void btnLoginOnMouseClicked(MouseEvent mouseEvent) {
+
+
+    public void btnLoginOnMouseClicked(MouseEvent mouseEvent) throws IOException {
 
             String username = txtUser.getText();
             String password = passwordPassword.getText();
@@ -65,9 +69,36 @@ public class LoginFormController {
             }
 
             if(errorsCount==0){
-                
+
+                try {
+                    String query = "select * from operation where username=? and password=?";
+                    pst = conn.prepareStatement(query);
+
+                    pst.setString(1,username);
+                    pst.setString(2,password);
+
+                    rs = pst.executeQuery();
+
+                    if(rs.next()){
+                        Parent parent = FXMLLoader.load(this.getClass().getResource("../view/DashboardForm.fxml"));
+                        Scene scene = new Scene(parent);
+                        Stage stage = (Stage) root.getScene().getWindow();
+
+                        stage.setScene(scene);
+                        stage.setTitle("Dashboard form");
+                        stage.centerOnScreen();
+                        stage.show();
 
 
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Are your sure if user is registered.");
+                    }
+
+
+
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
 
 
             }else{
