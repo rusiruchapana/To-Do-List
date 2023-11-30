731b6aa015d50bab3c379a232339af0f0183a839
rusiruchapana <rusiruchapana@gmail.com>
2023-11-30 07:36:54 +0530

    coding to dashboard.

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -1,40 +1,56 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import tm.ListViewTM;
 
 import java.io.IOException;
 
 public class DashboardFormController {
     public AnchorPane root;
-    public ListView listViewSet;
+    public ListView<ListViewTM> listViewSet;
     public TextField txtFieldTaskName;
     public Button btnLogout;
     public Button btnAddToList;
+    public Label lblTaskNameError;
+
+
 
 
     public void btnLogoutOnMouseClicked(MouseEvent mouseEvent) throws IOException {
-        Parent parent = FXMLLoader.load(this.getClass().getResource("../view/LoginForm.fxml"));
-        Scene scene = new Scene(parent);
-        Stage stage = (Stage) root.getScene().getWindow();
-        stage.setScene(scene);
-        stage.setTitle("Login form");
-        stage.centerOnScreen();
-        stage.show();
+            Parent parent = FXMLLoader.load(this.getClass().getResource("../view/LoginForm.fxml"));
+            Scene scene = new Scene(parent);
+            Stage stage = (Stage) root.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Login form");
+            stage.centerOnScreen();
+            stage.show();
+
+    }
+
+    public void btnAddToListOnMouseClicked(MouseEvent mouseEvent) {
+            String taskName = txtFieldTaskName.getText();
+
+
+            if(taskName.isEmpty()){
+                        lblTaskNameError.setText("Task Name field is empty.");
+
+            }else{
+                        ObservableList<ListViewTM> tasks = listViewSet.getItems();
+                        ListViewTM newListItem = new ListViewTM(taskName);
+                        tasks.add(newListItem);
+                        listViewSet.refresh();
 
 
 
 
-
-
-
+            }
 
     }
 }
---
+++ src/tm/ListViewTM.java
@@ -0,0 +1,20 @@
+package tm;
+
+public class ListViewTM {
+        private String lists;
+
+        public ListViewTM(String lists){
+                this.lists = lists;
+        }
+
+
+        public void setLists(String lists){
+                this.lists = lists;
+        }
+
+        public String getLists(){
+                return lists;
+        }
+
+
+}
